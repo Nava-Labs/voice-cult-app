919e52a0231dbc6c382761026d21f0fe09f74e5b
Ryan Tjhin <99659545+RyanTjhin@users.noreply.github.com>
2024-10-04 22:17:16 +0800

    feat: installed and implemented connectkit

---
+++ app/providers.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { WagmiProvider, createConfig } from "wagmi";
+import { mantaTestnet, scrollSepolia } from "wagmi/chains";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ConnectKitProvider, getDefaultConfig } from "connectkit";
+
+const config = createConfig(
+  getDefaultConfig({
+    // Your dApps chains
+    chains: [scrollSepolia, mantaTestnet],
+    // transports: {
+    //   // RPC URL for each chain
+    //   [mainnet.id]: http(
+    //     `https://eth-mainnet.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_ID}`,
+    //   ),
+    // },
+
+    // Required API Keys
+    walletConnectProjectId: "",
+
+    // Required App Info
+    appName: "Voice Cult",
+
+    // Optional App Info
+    appDescription: "",
+    appUrl: "", // your app's url
+    appIcon: "", // your app's icon, no bigger than 1024x1024px (max. 1MB)
+  }),
+);
+
+const queryClient = new QueryClient();
+
+export const Providers = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <ConnectKitProvider>{children}</ConnectKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
+};
